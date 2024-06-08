ba942f120a8710a0b7239751a589cdca52c1c1fe
sonu-dev <sonu.econnect@gmail.com>
2024-06-08 22:36:24 +0530

    initial commit

---
+++ Containerization/ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Containerization.ProductService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Containerization.ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(ILogger<ProductsController> logger) 
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = new List<Product> {
+                new Product(1, "Aspire3", "Accer", "Professional Laptop", 45000),
+                new Product(2, "Aspire5", "Accer", "Professional Laptop", 48000),
+                new Product(3, "Aspire7", "Accer", "Professional & Gaming Laptop", 53000),
+                new Product(4, "Nitro", "Accer", "Gaming Laptop", 65000),
+                new Product(5, "Nitro", "Predators", "Gaming Laptop", 75000),
+             };
+            return await Task.FromResult(StatusCode(StatusCodes.Status200OK, products));
+        }
+    }
+}
---
+++ Containerization/ProductService/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Containerization.ProductService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Brand {  get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; } = 0;
+
+        public Product(int id, string name, string brand, string description, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Brand = brand;
+            Description = description;
+            Price = price;
+        }
+    }
+}
